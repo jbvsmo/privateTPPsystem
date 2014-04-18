82dfaebb88063345c3447467bac3e2b374494438
Bernardo Vianna <jbvsmo@gmail.com>
2014-04-18 02:26:25 -0300

    Random names on a separated file

--- randomness.py
+++ randomness.py
@@ -1,26 +1,17 @@
 import random
 import string
 
+with open('data/random_names.txt', 'rb') as file:
+    random_names = file.read().decode('utf-8').strip().splitlines()
+
 
 def name(size, letters=string.ascii_lowercase):
     return ''.join(random.choice(letters) for _ in range(size))
 
 
 def select_name(do_not=(), size_random=7):
-    names = (
-        'Bob', 'Juan', 'Red', 'Blue', 'Oak', 'Mega Mewtwo X',
-        'Ray Charles', 'Jesse Pinkman', 'Walter White', 'Pikachu',
-        'Gary', 'Ash', 'Suzy', 'Misty', 'Brock', 'Punk', 'Tarzan',
-        'Smeagol', 'Ricardão', 'Hodor', 'Joffrey', 'Bruce Wayne',
-        'Wolverine', 'Mendigo', 'Capitão Caverna', 'Xuxa',
-        'Ted', 'Barney', 'Marshall', 'Lily', 'Robin', 'Batman',
-        'Gandalf', 'Frodo', 'Bilbo', 'Smaug', 'Garbodor',
-        'ABBBBBBK(', 'Jay Leno', 'Zapdos', 'Helix Fossil',
-        'Poochyena', 'Patrick', 'Jennifer Lawrence', 'AJ',
-        'Sherlock Holmes', 'Samuel L. Jackson', 'Yo Mama',
-    )
     for _ in range(5):
-        n = random.choice(names)
+        n = random.choice(random_names)
         if n not in do_not:
             return n
     return name(size_random)
